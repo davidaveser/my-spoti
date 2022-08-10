7720bacedf47e99b28fd42d8745cc41d538ad240
David Avelino <david.aveser@gmail.com>
2022-08-10 05:10:02 -0500

    Login page

---
+++ lib/constants/custom_colors.constants.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+abstract class CustomColors {
+  /// Color for the main baground of the views.
+  static Color background = const Color(0xFF151719);
+
+  /// Primary color.
+  static Color primary = const Color(0xFF551BF9);
+
+  /// Primary color (Dark).
+  static Color primaryDark = const Color(0xFF1B0178);
+
+  /// Spotify green color
+  static Color spotifyGreen = const Color(0xFF1CD464);
+
+  /// Spotify green color
+  static Color spotifyBlack = const Color(0xFF191414);
+}
---
+++ lib/ui/views/login.view.dart
@@ -0,0 +1,66 @@
+import 'package:blur/blur.dart';
+import 'package:flutter/material.dart';
+import 'package:my_spoti/constants/custom_colors.constants.dart';
+
+class LoginView extends StatelessWidget {
+  const LoginView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            // Widget header
+            Blur(
+              colorOpacity: 0.15,
+              borderRadius: const BorderRadius.only(
+                bottomLeft: Radius.circular(50.0),
+                bottomRight: Radius.circular(50.0),
+              ),
+              child: Container(
+                width: double.infinity,
+                height: 150.0,
+                decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.only(
+                    bottomLeft: Radius.circular(50.0),
+                    bottomRight: Radius.circular(50.0),
+                  ),
+                  gradient: LinearGradient(
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                    colors: [CustomColors.primaryDark, CustomColors.primary],
+                  ),
+                ),
+              ),
+            ),
+            const Spacer(flex: 3),
+
+            // Spotify icon
+            Image.asset('assets/general/spotify_icon_green.png', height: 60.0),
+
+            const SizedBox(height: 20.0),
+
+            // Log in button
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                primary: CustomColors.spotifyGreen,
+                shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(60.0))),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 100.0, vertical: 10.0),
+                child: Text(
+                  'Log in',
+                  style: TextStyle(color: CustomColors.spotifyBlack),
+                ),
+              ),
+            ),
+
+            const Spacer(flex: 4),
+          ],
+        ),
+      ),
+    );
+  }
+}
