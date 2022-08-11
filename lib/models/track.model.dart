976914d9fd372717bb1d8cb0b696c6c725252300
David Avelino <david.aveser@gmail.com>
2022-08-11 13:33:20 -0500

    Track model modifications

--- lib/models/track.model.dart
+++ lib/models/track.model.dart
@@ -1,35 +1,46 @@
+import 'package:my_spoti/models/album.model.dart';
+import 'package:my_spoti/models/artist.model.dart';
+
 /// Receive [data] as a json and returns a `List<Track>`
 List<Track> trackList(List<dynamic> data) => List<Track>.from(data.map<dynamic>((dynamic x) => Track.fromJson(x)));
 
 /// Model for Track intems.
 class Track {
   Track({
-    this.name,
-    this.trackNumber,
-    this.durationMs,
-    this.explicit,
+    required this.name,
+    required this.durationMs,
+    required this.trackNumber,
+    required this.isExplicit,
     this.previewUrl,
+    this.album,
+    this.artists,
   });
 
   factory Track.fromJson(Map<String, dynamic> json) => Track(
-        name: json['name'],
-        trackNumber: json['track_number'],
-        durationMs: json['duration_ms'],
-        explicit: json['explicit'],
+        name: json['name'] ?? '',
+        durationMs: json['duration_ms'] ?? 0,
+        trackNumber: json['track_number'] ?? -1,
+        isExplicit: json['explicit'] ?? false,
         previewUrl: json['preview_url'],
+        album: json['album'] == null ? null : Album.fromJson(json['album']),
+        artists: json['artists'] == null ? null : List<Artist>.from(json['artists'].map((dynamic x) => Artist.fromJson(x))),
       );
 
-  String? name;
-  int? trackNumber;
-  int? durationMs;
-  bool? explicit;
+  String name;
+  int durationMs;
+  int trackNumber;
+  bool isExplicit;
   String? previewUrl;
+  Album? album;
+  List<Artist>? artists;
 
   Map<String, dynamic> toJson() => <String, dynamic>{
         'name': name,
-        'track_number': trackNumber,
         'duration_ms': durationMs,
-        'explicit': explicit,
+        'track_number': trackNumber,
+        'explicit': isExplicit,
         'preview_url': previewUrl,
+        'album': album?.toJson(),
+        'artists': List<dynamic>.from(artists?.map<dynamic>((x) => x.toJson()) ?? <dynamic>[]),
       };
 }
