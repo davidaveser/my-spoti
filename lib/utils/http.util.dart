9827e3e5a94443888a752fcc11785c4fd0b378ff
David Avelino <david.aveser@gmail.com>
2022-08-11 19:58:18 -0500

    Adjutments for search repository

--- lib/utils/http.util.dart
+++ lib/utils/http.util.dart
@@ -14,14 +14,6 @@ abstract class HttpUtil {
   /// Return the required parameters for [SpotifyAPI.search].
   ///
   /// The String value to search [searchString] is required.
-  static Future<Map<String, dynamic>> spotifyParamsSearch(String searchString) async {
-    final String searchStringTrim = searchString.trimRight().trimLeft();
-
-    return <String, dynamic>{
-      'limit': '20',
-      'offset': '0',
-      'q': searchStringTrim.replaceAll(RegExp(r' '), '%20'),
-      'type': 'album,artist,track'
-    };
-  }
+  static Future<Map<String, dynamic>> spotifyParamsSearch(String searchString) async =>
+      <String, dynamic>{'limit': '20', 'offset': '0', 'q': searchString, 'type': 'album,artist,track'};
 }
